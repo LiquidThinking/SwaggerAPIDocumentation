87dee64e287243e9018d414eebee73949cadd3cc
Matthew Davies <matthew@spawtz.com>
2014-06-26 16:37:10 +0100

    Readded AssemblyFileVersion

--- ApiDocumentation/Properties/AssemblyInfo.cs
+++ ApiDocumentation/Properties/AssemblyInfo.cs
@@ -32,6 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion( "1.0.9.0" )]
+[assembly: AssemblyFileVersion( "1.0.9.0" )]
 [assembly: InternalsVisibleTo("SwaggerAPIDocumentationTests")]
 [assembly: InternalsVisibleTo("NUnit.Framework")]
 [assembly: InternalsVisibleTo("Rhino.Mocks")]
