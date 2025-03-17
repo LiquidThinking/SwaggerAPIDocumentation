b51039c13dd48812f5c2f7a1e162490525d8136d
warren-spawtz <41991703+warren-spawtz@users.noreply.github.com>
2025-03-17 12:26:30 +0000

    Update to netstandard2.0

--- ApiDocumentation/Implementations/ModelsGenerator.cs
+++ ApiDocumentation/Implementations/ModelsGenerator.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.Remoting;
 using System.Threading.Tasks;
 using SwaggerAPIDocumentation.Interfaces;
 using SwaggerAPIDocumentation.ViewModels;
