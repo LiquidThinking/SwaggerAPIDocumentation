0f33403dbacfa22911b32604ebaaced35156d06a
warren-spawtz <41991703+warren-spawtz@users.noreply.github.com>
2025-03-17 15:06:17 +0000

    Update to sdk style csprojs and to netstandard2.0 (#7)
    * Update to netstandard2.0
    
    * Add version
    
    * Add package Id

---
+++ ApiDocumentation/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("SwaggerAPIDocumentationTests")]
+[assembly: InternalsVisibleTo("NUnit.Framework")]
+[assembly: InternalsVisibleTo("Rhino.Mocks")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
